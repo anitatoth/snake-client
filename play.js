c164d37252067185d4e6b2bd93cfa15072b05308
Anita Toth <haluska99@hotmail.com>
2021-02-10 16:26:35 +0000

    updated event handler from quote to data

--- play.js
+++ play.js
@@ -12,7 +12,7 @@ const connect = function() {
   conn.setEncoding('utf8'); 
 
   conn.on('data', (data) => {
-    console.log('Server says: ', 'you ded cuz you idled');
+    console.log('Server says: ', data);
   });
   
 
